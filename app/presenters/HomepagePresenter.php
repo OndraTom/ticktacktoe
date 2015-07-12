04a6c7852d2e4ac78d8c32e5baa2433b6cd27c61
Ondrej Tom <ondrej.tom@joomlabs.cz>
2015-07-12 22:10:48 +0200

    base settup

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -2,19 +2,11 @@
 
 namespace App\Presenters;
 
-use Nette,
-	App\Model;
-
 
 /**
  * Homepage presenter.
  */
 class HomepagePresenter extends BasePresenter
 {
-
-	public function renderDefault()
-	{
-		$this->template->anyVariable = 'any value';
-	}
 
 }
